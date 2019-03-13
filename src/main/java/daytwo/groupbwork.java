2c28acbdff24bbdcd9bb040a9bf481d926f638d9
Chatyra <chatyrac@icloud.com>
2019-03-13 11:17:18 -0400

    My first commit

---
+++ src/main/java/daytwo/groupbwork.java
@@ -0,0 +1,11 @@
+package daytwo;
+
+public class groupbwork {
+    public static void main(String[] args) {
+                System.out.println(mult(100,8));
+            }
+
+            public static int mult(int y, int x){
+                return y * x;
+            }
+}
